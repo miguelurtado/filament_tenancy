ddbe0b24d75e1486fa7f7a2bdd3655c4628613a7
Miguel Urtado <miguelurtado@yahoo.com.br>
2023-10-18 18:16:59 -0300

    Add Departments to Team Model

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Team extends Model
 {
@@ -15,5 +16,10 @@ class Team extends Model
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function departments(): HasMany
+    {
+        return $this->hasMany(Department::class);
     }
 }
